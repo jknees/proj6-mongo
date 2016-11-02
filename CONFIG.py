4130eaa07c67ca41525fe6b52e456e4761eaf519
jknees <cortezcardinal@gmail.com>
2016-11-02 20:27:23 +0000

    Created and configured database and added more requirements

---
+++ CONFIG.py
@@ -0,0 +1,11 @@
+"""
+Configuration of memo app. 
+Generated Tue  1 Nov 23:20:24 UTC 2016
+Edit to fit development or deployment environment.
+
+"""
+
+PORT=5000
+DEBUG = True  # Set to False for production use
+secret_key="38eafbe7c8a5bb99dc8ca75eaac5eda7"
+
